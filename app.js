f9982c3db8dc5b751a6e8669853fa6ba19a41eb6
Ahmed <darwish144.ad@gmail.com>
2021-06-30 14:43:19 +0300

    News letter SignUp

---
+++ app.js
@@ -0,0 +1,17 @@
+
+//jshint esversion:7
+/* eslint quotes: ["error", "double"], curly: 2 */
+
+const express = ('express');
+const bodyParser = ('body-parser');
+const request = ('request');
+
+const app = express();
+
+
+app.listen(3000, function () {
+
+  console.log(' Server is running on port 3000');
+
+
+});
