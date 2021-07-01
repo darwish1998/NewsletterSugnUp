02518860896f20fbbc2f1869b9133b9279eb61d3
Ahmed <darwish144.ad@gmail.com>
2021-07-01 11:24:31 +0300

    API key and list IDs

--- app.js
+++ app.js
@@ -2,11 +2,32 @@
 //jshint esversion:7
 /* eslint quotes: ["error", "double"], curly: 2 */
 
-const express = ('express');
-const bodyParser = ('body-parser');
-const request = ('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.get("/", function(req, res) {
+
+  res.sendFile(__dirname + "/signup.html");
+
+
+});
+
+app.post("/", function(req , res){
+
+    var firstname = req.body.FName;
+    var lastname = req.body.LName;
+    var email = req.body.email;
+
+    console.log(firstname , lastname, email);
+
+
+});
 
 
 app.listen(3000, function () {
@@ -15,3 +36,9 @@ app.listen(3000, function () {
 
 
 });
+
+//API Key
+// 89eef207a58f636d5e541153726a2d3c-us6
+
+// ID List
+// 4c7fefd674
