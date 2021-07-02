cfd9046f80eaf800f683474ac3b5b1d868d5d641
Ahmed <darwish144.ad@gmail.com>
2021-07-02 10:44:22 +0300

    version 1

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 
 //jshint esversion:7
-/* eslint quotes: ["error", "double"], curly: 2 */
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-
+const https = require('https');
 const app = express();
 
 app.use(express.static("public"));
@@ -20,17 +19,70 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req , res){
 
-    var firstname = req.body.FName;
-    var lastname = req.body.LName;
-    var email = req.body.email;
+    const firstname = req.body.FName;
+    const lastname = req.body.LName;
+    const email = req.body.email;
 
-    console.log(firstname , lastname, email);
+    const data = {
+      members: [
+        {
+          email_address: email,
+          status:"subscribed",
+          merge_fields: {
+            FNAME: firstname,
+            LNAME: lastname,
+          }
+
+        }
+      ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+
+    const url =   "https://us6.api.mailchimp.com/3.0/lists/4c7fefd674"
+
+
+        const options   = {
+
+            method: "POST",
+            auth:"darwish144:89eef207a58f636d5e541153726a2d3c-us6"
+
+        }
+
+
+
+
+    const request = https.request(url, options, function(response) {
+
+    if (response.statusCode === 404) {
+
+      res.sendFile(__dirname + "/success.html");
+
+    }
+    else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+
+
+    })
+
+
+})
+request.write(jsonData);
+request.end();
 
 
 });
 
+app.post("/failure", function(req,res){
+  res.redirect("/");
+})
 
-app.listen(3000, function () {
+
+app.listen(process.env.PORT||3000, function () {
 
   console.log(' Server is running on port 3000');
 
